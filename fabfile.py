94c5a467301e6d8f533a41430738f545db6242a5
jeannirasic <jeannirasic@gmail.com>
2021-06-13 00:15:47 -0600

    Prueba final 11

--- fabfile.py
+++ fabfile.py
@@ -21,7 +21,7 @@ def funcionInstalar():
 	result=c.run("ng --version")
 	print("Instalo Docker")
 	result=c.run("sudo apt update")
-	result=c.run("sudo apt-get install \ apt-transport-https \ ca-certificates \ curl \ gnupg-agent \ software-properties-common")
+	result=c.run("sudo apt-get install \ apt-transport-https \ ca-certificates \ curl \ gnupg-agent \ software-properties-common --yes")
 	result=c.run("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
 	result=c.run("sudo add-apt-repository \ \"deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable\"")
 	result=c.run("sudo apt-get update")
